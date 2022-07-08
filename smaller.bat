51f4b205cacc5e19edc201d8a8af2275e9f55f66
MaksimKayumov <81908868+MaksimKayumov@users.noreply.github.com>
2022-07-08 21:36:27 +0500

    Update smaller.bat

--- smaller.bat
+++ smaller.bat
@@ -3,6 +3,6 @@ set v=%%
 echo @echo off>%1.bat
 echo :start>>%1.bat
 FOR /F "delims=; tokens=1,2,3,4" %%a in (%1) do (
-	echo set /a %%a=%v%%%b%v%+%%c>>%1.bat
-	echo if %%b == 0 goto start >>%1.bat
+	echo set /a %%a=%v%%%a%v%+%%b>>%1.bat
+	echo if %%a == 0 goto start >>%1.bat
 )
