26544133f459ad70f4e59910c6f97481e5eb12a3
MaksimKayumov <81908868+MaksimKayumov@users.noreply.github.com>
2022-07-12 16:35:00 +0500

    Added multeq

---
+++ multeq.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+set v=%%
+set cmd=0
+echo @echo off>%1.bat
+FOR /F "delims=; tokens=1,2,3,4" %%a in (%1) do (
+	set /a cmd=!cmd!+1
+	echo :!cmd!>>%1.bat
+	echo set /a %%a=%v%%%a%v%*%%b+%%c>>%1.bat
+	echo goto %%d>>%1.bat
+)
